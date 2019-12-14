7cdfc8ba11ef216027325649abadfed949192a98
shotasan <wondermoai@gmail.com>
2019-12-15 02:59:03 +0900

    2-6 リファクタリング

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const http = require("http");
 const fs = require("fs");
 
-// http.Serverオブジェクトの作成
-var server = http.createServer(
-  (request, response) => {
-    // readFile(ファイルパス,エンコーディング名,コールバック関数)
-    fs.readFile("./index.html", "UTF-8",
-      (error, data) => {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        response.end();
-      });
-  }
-);
+var server = http.createServer(getFromClient);
+
+// createServerの処理を切り出し
+function getFromClient(req, res) {
+  request = req;
+  response = res;
+  fs.readFile("./index.html", "UTF-8",
+    (error, data) => {
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(data);
+      response.end();
+    }
+  );
+}
+
+
 server.listen(3000);
 console.log("Server start!");
