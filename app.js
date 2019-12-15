793ad142d09795c9b161295d108f2679aa851532
shotasan <wondermoai@gmail.com>
2019-12-15 17:07:41 +0900

    2-16 スタイルシートの読み込み処理を追加

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require("http");
 const fs = require("fs");
-// ejsオブジェクトの読み込み
 const ejs = require("ejs");
+// urlオブジェクトの作成
+const url = require("url");
 
-// テンプレートファイルの読み込み
-// サーバー実行前に同期処理でファイルを読みこむ
 const index_page = fs.readFileSync("./index.ejs", "utf8");
+// スタイルファイルの読み込み
+const style_css = fs.readFileSync("./style.css", "utf8");
 
 var server = http.createServer(getFromClient);
 
@@ -13,13 +14,27 @@ server.listen(3000);
 console.log("Server start!");
 
 function getFromClient(request, response) {
-  // レンダリングの実行（HTMLファイルへの変換）
-  // renderの第二引数にオブジェクトを指定し、ejfファイルに値を渡す
-  var content = ejs.render(index_page, {
-    title: "Index",
-    content: "これはテンプレートを使ったサンプルページです。"
-  });
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(content);
-  response.end();
+  var url_parts = url.parse(request.url);
+  // pathnameプロパティから/以下のパスを取得する
+  switch (url_parts.pathname) {
+    case "/":
+      var content = ejs.render(index_page, {
+        title: "Index",
+        content: "これはテンプレートを使ったサンプルページです。"
+      });
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+      break;
+    // index.ejsのlinkからstyle.cssにアクセスした際にcssが帰るようにする
+    case "/style.css":
+      response.writeHead(200, { "Content-Type": "text/css" });
+      response.write(style_css);
+      response.end();
+      break;
+    default:
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("no page...");
+      break;
+  }
 }
