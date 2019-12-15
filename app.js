a490371c91b4606aaa53503417bbe864a92cb7b4
shotasan <wondermoai@gmail.com>
2019-12-15 13:03:12 +0900

    2-9 htmlの動的な変更

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 const http = require("http");
 const fs = require("fs");
-// グローバル変数を定義
-var request;
-var response;
 
 var server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log("Server start!");
 
-// createServerの処理を切り出し
-function getFromClient(req, res) {
-  request = req;
-  response = res;
-  fs.readFile("./index.html", "UTF-8", writeToResponse);
-}
+function getFromClient(request, response) {
+  fs.readFile("./index.html", "UTF-8",
+    (error, data) => {
+      var content = data.
+        replace(/dummy_title/g, "タイトルです").
+        replace(/dummy_content/g, "これがコンテンツです。");
 
-// readFileのコールバック関数
-function writeToResponse(error, data) {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(data);
-  response.end();
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(content);
+      response.end();
+    }
+  );
 }
-
