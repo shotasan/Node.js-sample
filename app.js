552a01927cb1c1912fb357cfa9ce6a809f51d1ea
shotasan <wondermoai@gmail.com>
2019-12-16 14:36:04 +0900

    3-7,3-8,3-9 パーシャルの利用

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ function response_index(request, response) {
   var content = ejs.render(index_page, {
     title: "Index",
     content: msg,
-    data: data
+    data: data,
+    filename: 'data_item'
   });
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write(content);
@@ -58,20 +59,14 @@ function response_index(request, response) {
 function response_other(request, response) {
   var msg = "これはOtherページです。"
 
-  // POST送信時の処理を記載
   if (request.method == "POST") {
-    // 空の文字列を定義
     var body = '';
 
-    // dataイベント:クライアントからデータを受け取ると発生するイベント
-    // 受け取ったdataをbodyに追加
     request.on('data', (data) => {
       body += data;
     });
 
-    // endイベント:データの受け取りが完了すると発生するイベント
     request.on('end', () => {
-      // 受け取ったデータをパースしてテキストの値として取り出す
       var post_data = qs.parse(body);
       msg += `あなたは「${post_data.msg}」と書きました。`;
       var content = ejs.render(other_page, {
